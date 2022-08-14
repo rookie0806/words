81433d35fef525225b8844c853368cc36c42a42c
rookie0806 <rookie0806@naver.com>
2022-08-14 23:16:22 +0900

    first commit msg

---
+++ frontend/build/precache-manifest.122381185693ba32b3926ace57a801d4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0baa6a95dc00af6cc35c64b9a302e31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a557130f2e6906df0a41",
+    "url": "/static/css/2.6a2d8eaa.chunk.css"
+  },
+  {
+    "revision": "785f88e421b403931feb",
+    "url": "/static/css/main.862dbd65.chunk.css"
+  },
+  {
+    "revision": "a557130f2e6906df0a41",
+    "url": "/static/js/2.9aa868df.chunk.js"
+  },
+  {
+    "revision": "9350487abadd8d11f850928dfc28bf58",
+    "url": "/static/js/2.9aa868df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785f88e421b403931feb",
+    "url": "/static/js/main.c7409054.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "2fed9f13ba2c7b89de9d2211f0e48d6e",
+    "url": "/static/media/SCDream1.2fed9f13.ttf"
+  },
+  {
+    "revision": "f26262d09fee51b4f84ac83e41b1b01d",
+    "url": "/static/media/SCDream2.f26262d0.ttf"
+  },
+  {
+    "revision": "8f50903bbbd9f873931e98f010e62609",
+    "url": "/static/media/SCDream3.8f50903b.ttf"
+  },
+  {
+    "revision": "a8d22f21f0ffbf92431e697baf2398b4",
+    "url": "/static/media/SCDream4.a8d22f21.ttf"
+  },
+  {
+    "revision": "5eca7ea12116a89a77006bdede97ac1f",
+    "url": "/static/media/SCDream6.5eca7ea1.ttf"
+  },
+  {
+    "revision": "37cb1bc876bb680dca657ab45cadeeba",
+    "url": "/static/media/SCDream7.37cb1bc8.ttf"
+  },
+  {
+    "revision": "51b157502d4e93a2b7029698cbe3bbed",
+    "url": "/static/media/SCDream8.51b15750.ttf"
+  },
+  {
+    "revision": "7271da6c1e2f63850bf2d239346ec6a7",
+    "url": "/static/media/SCDream9.7271da6c.ttf"
+  },
+  {
+    "revision": "69d943d44529458a6cc60e58ecfa90f3",
+    "url": "/static/media/logo.69d943d4.png"
+  }
+]);
